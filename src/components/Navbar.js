3d2dd6e01bba053fc4acce5bb6af8ad3edbc1c5b
Binshi <binshiferoke@gmail.com>
2022-04-12 12:04:17 +0530

    Banner Created

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className='nav'>
+        <img className='netflix' src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
+   <img className='usericon' src="https://archive.org/download/profiles_202104/hero.png" alt="" />
+    </div>
+  )
+}
+
+export default Navbar
