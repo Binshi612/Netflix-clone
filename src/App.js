978c890d272c0a95be24cdce296331aec948e97e
Binshi <binshiferoke@gmail.com>
2022-04-18 11:37:40 +0530

    slider created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import Slider from "./components/Slider";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner />
+      <Slider />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Card.css";
+
+function Card() {
+  return (
+    <div className="card">
+      {/* <h1 className="head">title</h1>  */}
+      <img
+        src="https://offloadmedia.feverup.com/secretldn.com/wp-content/uploads/2020/03/18074959/72354623_743029929493734_4704406170316788179_n-1024x912.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Card;
